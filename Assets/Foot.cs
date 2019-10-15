0eb02f660baac7271632bb625c91ab75a9c980c3
BlueBug <iopzxz@gmail.com>
2019-10-16 04:35:58 +0900

    m

---
+++ Assets/Foot.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Foot : MonoBehaviour
+{
+	[SerializeField]
+	Transform attachedBody;
+	[SerializeField]
+	float maximumLength = 1.0f;
+	/// <summary>
+	/// Targetting object can lost in case foot losess its connection
+	/// </summary>
+	Transform targettingObject;
+
+	void attach(Transform target, float breakLimit = 1)
+	{
+
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+	public void loseTarget()
+	{
+		targettingObject = null;
+	}
+	private void FixedUpdate()
+	{
+		var distance = attachedBody.transform.position-this.transform.position  ;
+		var mag = distance.magnitude;
+		if(mag > maximumLength)
+		{
+			//foot cannot hold the object anymore
+			loseTarget();
+			var dir = distance.normalized;
+			this.transform.position = attachedBody.transform.position+dir * maximumLength;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ Assets/Slug.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Slug : MonoBehaviour
+{
+	public Vector2 TargetPosition = new Vector2(-10, 0);
+	public List<Foot> foots;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+		//scan the surrounding 
+		//use unused foots first 
+		//unlink lowest performing foor to locate towards better pefroming situations
+    }
+}
