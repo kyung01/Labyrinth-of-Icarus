f7df0c4e33906d136b66dfe41fb98cfb4c769f20
BlueBug <iopzxz@gmail.com>
2019-10-16 04:11:02 +0900

    Added Map generator

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -22,12 +22,15 @@ namespace KWorld {
 			for(int i = 0; i < 4; i++)
 			{
 
-				var rroomList = new List<Room>();
+				var roomList = new List<Room>();
 				for(int j = 0; j < 4; j++)
 				{
-					rroomList.Add(new Room( new Vector2(i,j) ));
+					roomList.Add(new Room( new Vector2(j,i) ));
 				}
+				rooms.Add(roomList);
 			}
+
+
 			for(int i = 0; i < 4; i++)
 			{
 				for(int j = 0; j < 4; j++)
@@ -36,19 +39,21 @@ namespace KWorld {
 					{
 						for(int y= 0; y < 20; y++)
 						{
-
-							rooms[i][j].tile.Add(tiles[20 * i + y][20 * j + x]);
+							//Debug.Log(""+rooms[i][j].tiles+ 20 * i + y + " , " + 20 * j + x + " , " + tiles[20 * i + y][20 * j + x]);
+							rooms[i][j].tiles.Add(tiles[20 * i + y][20 * j + x]);
 						}
 					}
 				}
 			}
 			
 		}
-		bool hprIsRoomEndRoomFloor(Room room)
+		bool isValidRoomConnectionPosition(Vector2 roomPosition)
 		{
-			return false;
+			if (!isVallidRoomPosition(roomPosition)) return false;
+			if (!rooms[(int)roomPosition.y][(int)roomPosition.x].isAllDoorsClosed()) return false;
+			return true;
 		}
-		bool hprIsPositionCorrectRoomPosition(Vector2 position)
+		bool isVallidRoomPosition(Vector2 position)
 		{
 			return !(position.x < 0.0f || position.y < 0.0f ||
 				position.y >= rooms.Count || position.x >= rooms[(int)position.y].Count
@@ -66,18 +71,34 @@ namespace KWorld {
 			int selectedRoomIndex = Random.Range(0, 4);
 			var selectedRoom = rooms[3][selectedRoomIndex];
 			selectedRoom.open(false,true,true,true);
-			do {
+			do
+			{
+				Debug.Log("Room is at " + selectedRoom.ToString());
 				var movementList = new List<Vector2>();
+				bool isUPOpenable = isValidRoomConnectionPosition(selectedRoom.position + new Vector2(0, +1));
+				bool isLeftOpenable = isValidRoomConnectionPosition(selectedRoom.position + new Vector2(-1, 0));
+				bool isDownOpenable = isValidRoomConnectionPosition(selectedRoom.position + new Vector2(0, -1));
+				bool isRIghtOpenable = isValidRoomConnectionPosition(selectedRoom.position + new Vector2(1, 0));
+				selectedRoom.open(isUPOpenable, isRIghtOpenable, isDownOpenable, isLeftOpenable);
+
 				if (selectedRoom.openLeft) movementList.Add(new Vector2(-1, 0));
 				if (selectedRoom.openRight) movementList.Add(new Vector2(1, 0));
 				if (selectedRoom.openDown) movementList.Add(new Vector2(0, -1));
+				if (movementList.Count == 0)
+				{
+					Debug.Log("Unavailalbe movment detected");
+					break;
+				}
 				Vector2 newSelectedRoomPoision = Vector2.zero;
 				int randomChosenPath = Random.Range(1, 100) % movementList.Count;
 				bool newRoomIsChosen = false;
 				for (int i = 0; i <3; i++)
 				{
-					newSelectedRoomPoision = selectedRoom.position+  movementList[randomChosenPath];
-					if (hprIsPositionCorrectRoomPosition(newSelectedRoomPoision))
+					//Debug.Log("newSelectedRoomPoision = " + selectedRoom.position + " + " + movementList[randomChosenPath]);
+					newSelectedRoomPoision = selectedRoom.position +  movementList[randomChosenPath];
+					//Debug.Log("newSelectedRoomPoision = " + newSelectedRoomPoision);
+					if (isVallidRoomPosition(newSelectedRoomPoision)&&
+						 rooms[(int)newSelectedRoomPoision.y][(int)newSelectedRoomPoision.x].isAllDoorsClosed())
 					{
 						newRoomIsChosen = true;
 						break;
@@ -86,7 +107,7 @@ namespace KWorld {
 				if (newRoomIsChosen)
 				{
 					selectedRoom = rooms[(int)newSelectedRoomPoision.y][(int)newSelectedRoomPoision.x];
-
+					//Debug.Log("Selecting room " + selectedRoom.ToString());
 				}
 				else
 				{
@@ -94,7 +115,7 @@ namespace KWorld {
 					Debug.Log("Map creaion failed ");
 					break;
 				}
-			} while (hprIsRoomEndRoomFloor(selectedRoom));
+			} while (true);
 		}
 		private void Awake()
 		{
--- Assets/Scripts/room.cs
+++ Assets/Scripts/room.cs
@@ -4,36 +4,50 @@ using System.Collections.Generic;
 
 namespace KWorld
 {
-public class Room
-{
+	public class Room
+	{
 		public Vector2 position;
-		public List<Tile> tile;
-		public bool openUP,openRight, openDown, openLeft;
-		
-		public Room (Vector2 position)
+		public List<Tile> tiles = new List<Tile>();
+		public bool openUP, openRight, openDown, openLeft;
+
+		public Room(Vector2 position)
 		{
 			this.position = position;
 		}
-	// Use this for initialization
-	void Start()
-	{
-
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-
-	}
-		public void open(bool openUp,bool openRight, bool openDown,bool openLeft)
+		// Use this for initialization
+		void Start()
 		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+		public void open(bool openUp, bool openRight, bool openDown, bool openLeft)
+		{
+			this.openUP = openUp;
+			this.openLeft = openLeft;
+			this.openDown = openDown;
+			this.openRight = openRight;
 
 		}
 		public void close()
 		{
 			open(false, false, false, false);
 		}
-}
+		public bool isAllDoorsClosed()
+		{
+			return !openUP && !openRight && !openLeft && !openDown;
+		}
+		public override string ToString()
+		{
+			return "ID[" + (int)position.x + " , " + (int)position.y +"] "+ openUP + "/" + openRight + "/" + openDown + "/" + openLeft;
+		}
+
+	}
+
 
 
 }
