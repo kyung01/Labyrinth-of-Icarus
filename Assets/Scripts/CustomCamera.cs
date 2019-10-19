cf452f3bdc39253c05023b44f0498fb202ee7dcd
BlueBug <iopzxz@gmail.com>
2019-10-19 16:53:37 +0900

    Added main menu

---
+++ Assets/Scripts/CustomCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CustomCamera : MonoBehaviour
+{
+	[SerializeField]
+	float width;
+	Camera camera;
+	private void Awake()
+	{
+		camera = GetComponent<Camera>();
+	}
+	// Start is called before the first frame update
+	void Start()
+    {
+		adjustCameraSize();
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+        
+    }
+
+	void adjustCameraSize()
+	{
+		float orthographicSize = ((float)Screen.height / Screen.width) * (width/2.0f);
+		if (orthographicSize < 15.0f) camera.orthographicSize = 15.0f;
+		else camera.orthographicSize = orthographicSize;
+	}
+
+}
