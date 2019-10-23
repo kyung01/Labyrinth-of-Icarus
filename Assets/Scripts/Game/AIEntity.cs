dd64da670fc10dc1f44aac55d76da1dc5493e60d
BlueBug <iopzxz@gmail.com>
2019-10-23 17:19:00 +0900

    Added meatball

--- Assets/Scripts/Game/AIEntity.cs
+++ Assets/Scripts/Game/AIEntity.cs
@@ -7,6 +7,7 @@ using System.Collections;
 public class AIEntity : Entity
 {
 	public enum EntityState {IDL,PATROLLING,FOUND_TARGET,ENGAGED };
+	protected EntityState state = EntityState.IDL;
 	// Use this for initialization
 	void Start()
 	{
@@ -15,6 +16,35 @@ public class AIEntity : Entity
 
 	// Update is called once per frame
 	void Update()
+	{
+		switch (state) {
+			case EntityState.IDL:
+				updateIDL();
+				break;
+			case EntityState.PATROLLING:
+				updatePatrolling();
+				break;
+			case EntityState.ENGAGED:
+				updateEngaged();
+				break;
+			case EntityState.FOUND_TARGET:
+				updateFoundTarget();
+				break;
+		}
+	}
+	public virtual void updateIDL()
+	{
+
+	}
+	public virtual void updateFoundTarget()
+	{
+
+	}
+	public virtual void updatePatrolling()
+	{
+
+	}
+	public virtual void updateEngaged()
 	{
 
 	}
--- Assets/Scripts/Game/Creatures/MeatBall.cs
+++ Assets/Scripts/Game/Creatures/MeatBall.cs
@@ -4,16 +4,88 @@ using System.Collections;
 //
 public class MeatBall : AIEntity
 {
+	[SerializeField]
+	float detectTimeInterval = 1;
+	[SerializeField]
+	float detectRange = 1;
+	[SerializeField]
+	float detectJumpForce = 1;
+	[SerializeField]
+	float reactionTime = 1;
+	[SerializeField]
+	float rollingTorque = 1;
+	[SerializeField]
+	float torqueApplyInterval = 1;
 
-	// Use this for initialization
-	void Start()
+	float timeRemainingToReact = 0;
+
+	float timeRemainingToDetect = 0;
+
+	float timeRemainingToApplyTorque = 0;
+
+	private void Start()
 	{
-
+		timeRemainingToDetect = detectTimeInterval;
+		timeRemainingToReact = reactionTime;
+		timeRemainingToApplyTorque = torqueApplyInterval;
+	}
+	/// <summary>
+	/// MeatBall jumps every few 2~3 seconds until approached by the player
+	/// Then Once player appraoches the meatball, meatball becomes alert
+	/// </summary>
+	public override void updateIDL()
+	{
+		base.updateIDL();
+		timeRemainingToDetect -= Time.deltaTime;
+		if (timeRemainingToDetect < 0)
+		{
+			timeRemainingToDetect = detectTimeInterval;
+			detectPlayer();
+		}
+	}
+	void detectPlayer()
+	{
+		var dis = Player.GetPlayerPosition() - this.transform.position;
+		if(dis.sqrMagnitude < detectRange* detectRange)
+		{
+			//detected the player
+			this.state = EntityState.FOUND_TARGET;
+			rigidbody.AddForce(this.transform.up * detectJumpForce, ForceMode2D.Impulse);
+		}
+	}
+	/// <summary>
+	/// Once MeatBall finds the targe, it waits certain time period, to give the player time to react
+	/// </summary>
+	public override void updateFoundTarget()
+	{
+		base.updateFoundTarget();
+		timeRemainingToReact -= Time.deltaTime;
+		if(timeRemainingToReact < 0)
+		{
+			timeRemainingToReact = reactionTime;
+			state = EntityState.ENGAGED;
+		}
+	}
+	/// <summary>
+	/// Meatball rolls to the direction of the player by using torque 
+	/// </summary>
+	public override void updateEngaged()
+	{
+		base.updateEngaged();
+	}
+	private void FixedUpdate()
+	{
+		if(state == EntityState.ENGAGED)
+		{
+			timeRemainingToApplyTorque -= Time.fixedDeltaTime;
+			if (timeRemainingToApplyTorque < 0)
+			{
+				timeRemainingToApplyTorque = torqueApplyInterval;
+				float x = Player.GetPlayerPosition().x - this.transform.position.x;
+				rigidbody.AddTorque(-x / Mathf.Abs(0.01f + x) * rollingTorque , ForceMode2D.Impulse);
+			}
+			
+		}
 	}
 
-	// Update is called once per frame
-	void Update()
-	{
-
-	}
 }
